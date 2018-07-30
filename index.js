d22b2be93063f6ede89055cd885113072676b34e
DrMegavolt <sergeykuc@gmail.com>
2018-07-30 15:16:50 +0300

    listen to configstore

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const debug = require('debug')('sls:main');
+require('./lib/git-listener')();
 const kube = require('./lib/kube');
 kube.ensureConfig(); // Loading ~/.kube/config based on k8s run secret for serviceAccount
 
