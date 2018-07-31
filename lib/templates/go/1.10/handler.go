9a2b35c955e151d70a745ba8bfe8777ce1ce140a
DrMegavolt <sergeykuc@gmail.com>
2018-07-31 18:42:56 +0300

    bugfix; Go lang support

--- lib/templates/go/1.10/handler.go
+++ lib/templates/go/1.10/handler.go
@@ -4,6 +4,6 @@ import (
 	"github.com/kubeless/kubeless/pkg/functions"
 )
 
-func FN_CAMEL_NAME(event functions.Event, context functions.Context) (string, error) {
+func Handler(event functions.Event, context functions.Context) (string, error) {
 	return "LunchBadger Go 1.10 function", nil
 }
