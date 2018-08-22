f83b60c34340084ea2fa72db0ab39634895dc2e7
DrMegavolt <sergeykuc@gmail.com>
2018-08-22 13:25:19 +0300

    java text fix

--- lib/templates/java/1.8/handler.java
+++ lib/templates/java/1.8/handler.java
@@ -6,6 +6,6 @@ import io.kubeless.Context;
 public class handler {
     public String FN_NAME(io.kubeless.Event event, io.kubeless.Context context) {
         System.out.println(event.Data);
-        return event.Data;
+        return "LunchBadger Java function"; // event.Data;
     }
 }
